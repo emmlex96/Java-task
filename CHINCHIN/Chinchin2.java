6532683123ce4f069b1bc756f7eebfabefaaf9a3
emmlex96 <emmlex96@gmail.com>
2026-04-06 16:50:20 +0100

    Chinchin

---
+++ CHINCHIN/Chinchin2.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+public class Chinchin2 {
+
+public static void main(String[] args) {
+
+Scanner apple = new Scanner(System.in);
+
+System.out.print(" Enter Month :");
+
+String month = apple.nextLine();
+
+
+if (month.equals ("january")) {
+
+System.out.print(" 31 days ");
+}
+if (month.equals ("february")) {
+
+System.out.print(" 28 or 29 days ");
+}
+if (month.equals ("march")) {
+
+System.out.print(" 31 days ");
+}
+if (month.equals ("april")) {
+
+System.out.print(" 30 days ");
+}
+if (month.equals ("may")) {
+
+System.out.print(" 31 days ");
+}
+if (month.equals ("june")) {
+
+System.out.print(" 30 days ");
+}
+if (month.equals ("july")) {
+
+System.out.print(" 31 days ");
+}
+if (month.equals ("august")) {
+
+System.out.println("31 days");
+}
+if (month.equals ("september")) {
+
+System.out.println("30 days");
+}
+if (month.equals ("october")) {
+
+System.out.println("31 days");
+}
+if (month.equals ("november")) {
+
+System.out.println("30 days");clear
+}
+if (month && year.equals ("december ")){ 
+
+System.out.println("31 days + 2026 ");
+}
+}
+
+}
+
---
+++ CHINCHIN/Chinchin5.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Chinchin5 {
+            public static void main (String[] args) {
+                        Scanner apple = new Scanner(System.in);
+
+//                      User instructions
+                        System.out.println("Pick a number between 0 and 1 to play this game of flipping a coin");
+                        int playerNum = apple.nextInt();
+
+//                      Generating a number between 0 and 1
+                         int randomNum = (int)(Math.random() * 1);
+
+//                      coin sides
+                         String up = "Head";
+                         String down = "Tail";
+
+//                      checking player input
+                        if(randomNum == 1) {
+                            System.out.printf("No chosen = %d : Comp chose = %d: %s %n%n' You Won!!! %n",playerNum,randomNum,up);
+                        } else if(randomNum == 0) {
+                            System.out.printf("No chosen = %d : Comp chose = %d: %s %n%n' You lost!!! %n",playerNum,randomNum,down);
+                        } else if (playerNum != 1 && playerNum>1 ) {
+}
+}
+                         
+            }
+
