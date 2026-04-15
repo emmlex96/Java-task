16c38cdecef4c3cf1e16fc9f24fb584c1f90b0ed
emmlex96 <emmlex96@gmail.com>
2026-04-15 17:28:53 +0100

    Add files via upload

---
+++ FIRE DRILL 2/TaskTwo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+    public class TaskOne{
+    public static void main(String[]args){
+Scanner inputCollector = new Scanner(System.in);
+int Total = 0;
+int count = 1;
+while (count <= 10){
+System.out.print("Enter score ");
+int score = inputCollector.nextInt();
+Total =Total + score ;
+count++;
+}
+int average = Total/10;
+System.out.println(average);
+}
+}
