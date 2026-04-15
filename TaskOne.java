7ce365f875682a1c4820c84e192d1c8f6557358e
emmlex96 <emmlex96@gmail.com>
2026-04-15 17:12:49 +0100

    Add files via upload

---
+++ TaskOne.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+    public class TaskOne{
+    public static void main(String[]args){
+Scanner inputCollector=new Scanner(System.in);
+System.out.println("Enter ten score");
+int billy = inputCollector.nextInt();
+if (sum == total of score);
+System.out.print("the sum of the score is: " + sum);
+}
+}
