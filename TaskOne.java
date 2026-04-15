e3a5285ae270ac562353931b0230d33e41410b7a
emmlex96 <emmlex96@gmail.com>
2026-04-15 17:24:14 +0100

    Add files via upload

--- TaskOne.java
+++ TaskOne.java
@@ -1,10 +1,15 @@
 import java.util.Scanner;
     public class TaskOne{
     public static void main(String[]args){
-Scanner inputCollector=new Scanner(System.in);
-System.out.println("Enter ten score");
-int billy = inputCollector.nextInt();
-if (sum == total of score);
-System.out.print("the sum of the score is: " + sum);
+Scanner inputCollector = new Scanner(System.in);
+int Total = 0;
+int count = 1;
+while (count <= 10){
+System.out.print("Enter score ");
+int score = inputCollector.nextInt();
+Total =Total + score ;
+count++;
+}
+System.out.println("the sum of score is :" + Total);
 }
 }
